026357edbe92a97fd3ffc71e55e003f9b1124666
Cesar Avalos <cesar.avalos3@gmail.com>
2015-01-18 11:02:03 -0600

    Initial Commit

--- CustomGravity.js
+++ CustomGravity.js
@@ -5,13 +5,13 @@ var gravSource:transform;
 
 function CustomGravity()
 {
-				var gravityDirection:Vector3 = (gravSource.transform.position - transform.position).normalized;
-				rigidbody.AddForce((gravSource.transform.position - transform.position).normalized * 10);
-				var rotation:Quaternion = Quaternion.FromToRotation(transform.up,gravityDirection) * transform.rotation;
-				transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 50 * Time.deltaTime);
+	var gravityDirection:Vector3 = (gravSource.transform.position - transform.position).normalized;
+	rigidbody.AddForce((gravSource.transform.position - transform.position).normalized * 10);
+	var rotation:Quaternion = Quaternion.FromToRotation(transform.up,gravityDirection) * transform.rotation;
+	transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 50 * Time.deltaTime);
 }
 
 function Update()
 {
-		CustomGravity();
+	CustomGravity();
 };
